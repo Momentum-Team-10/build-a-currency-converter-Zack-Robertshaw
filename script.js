6c7bd5aad2acd58a23a8b9787beb71891412153d
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-13 22:17:36 -0400

    not much to look at in the browser but I understand what's going on and how to make it better

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('hooked up?')
+
 const currencies = [
   'EUR',
   'CAD',
@@ -33,4 +35,37 @@ const currencies = [
   'KRW',
   'MYR'
 ]
+
+let root = document.getElementById('root')
+
+let container = document.createElement('div')
+container.id = 'container'
+root.appendChild(container)
+
+let dropDown = document.createElement('select')
+container.appendChild(dropDown)
+
+// for loop for dropdown list
+for (let currency of currencies) {
+  let option = document.createElement('option')
+  option.innerText = currency
+  dropDown.appendChild(option)
+
+}
+// this is where the dropdown choice gets pulled out
+// I think there's some stuff out of order that needs to be higher up
+let inputCurrencyName = document.createElement('label')
+inputCurrencyName.id = 'inputCurrencyName'
+dropDown.appendChild(inputCurrencyName)
+
+
+dropDown.addEventListener('change', () => {
+  inputCurrencyName.innerText = dropDown.value;
+})
+
+// fetch succesfully consoles data
+fetch('https://openexchangerates.org/api/latest.json?app_id=c82922b826df4a358dbc0387ffd36c5b')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
 
